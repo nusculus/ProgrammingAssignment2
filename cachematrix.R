821285c8dacde61945f10a22a7f1a8941c8f5c07
Kevin Matthew Nuss <github@nusculus.com>
2016-12-25 21:17:38 -0600

    Completed matrix caching functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These are functions to create and access an object containing a 
+## matrix and its inverse. To save processing time, the matrix inverse is
+## cached when possible for future accesses.
 
-## Write a short comment describing this function
+## Create, initialize, and return the object to store and access the matrix
+# and its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(cached_matrix = matrix()) {
+        
+        matrix_inverse <- NULL
+        
+        set <- function(new_matrix) {
+                cached_matrix <<- new_matrix
+                matrix_inverse <<- NULL
+        }
+        
+        get <- function() {
+                cached_matrix
+        }
+        
+        getinverse <- function() {
+                matrix_inverse
+        }
+        
+        setinverse <- function(new_inverse) {
+                matrix_inverse <<- new_inverse
+        }
+        
+        list(set = set, get = get, setinverse = setinverse, 
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Calculate, store, and return the inverse of a makeCacheMatrix matrix 
+## and return it. If the inverse has already been calculated and cached, then
+## return that that stored value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        the_matrix <- x$get()
+        if(is.na(the_matrix[1][1])) {
+                stop("Atempting to retrieve inverse of an empty matrix")
+        }
+        
+        result <- x$getinverse()
+        if(is.null(result)) {
+                result <- solve(the_matrix, ...)
+                x$setinverse(result)
+        } else {
+                message("getting cached data")
+        }
+
+        result
 }
